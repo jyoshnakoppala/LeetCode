192d42c75dc86c4adb0734dfa966b570fb50cd58
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-05-25 11:33:22 -0400

    Time: 12 ms (87.20%) | Memory: 43 MB (39.01%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,19 +1,59 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int count=1;
-        StringBuilder temp=new StringBuilder(a);
-        while(temp.length()<b.length())
-        {
-            temp.append(a);
+         StringBuilder sb = new StringBuilder(a);
+        int count = 1;
+
+        while (sb.length() < b.length()) {
+            sb.append(a);
             count++;
         }
-        if(temp.toString().contains(b))
-        return count;
 
-        temp.append(a);
-        if(temp.toString().contains(b))
-        return count+1;
+        if (kmpSearch(sb.toString(), b)) return count;
+
+        sb.append(a);
+        if (kmpSearch(sb.toString(), b)) return count + 1;
 
         return -1;
+    }
+
+    // KMP search function
+    private boolean kmpSearch(String text, String pattern) {
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if (j == pattern.length()) return true;
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Build LPS (Longest Prefix Suffix) array
+    private int[] buildLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0, i = 1;
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i++] = len;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i++] = 0;
+                }
+            }
+        }
+        return lps;
     }
 }
