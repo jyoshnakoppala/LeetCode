829343745cff4390c81ac495ec8381b775dc2e99
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-06-02 23:23:39 -0400

    Time: 3 ms (85.01%) | Memory: 45.9 MB (45.78%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,29 +1,30 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] candies = new int[n];
-        Arrays.fill(candies, 1);
+       int n = ratings.length;
+        int[] left = new int[n];
 
-        // Left to Right
+        // Left to right pass
+        left[0] = 1;
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
             }
         }
 
-        // Right to Left
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        // Right to left pass using single variable
+        int cur = 1;
+        int sum = Math.max(left[n - 1], cur);  // Include last child's candy
+        for (int j = n - 2; j >= 0; j--) {
+            if (ratings[j] > ratings[j + 1]) {
+                cur += 1;
+            } else {
+                cur = 1;
             }
+            sum += Math.max(left[j], cur);
         }
 
-        // Total
-        int total = 0;
-        for (int c : candies) {
-            total += c;
-        }
-
-        return total;
+        return sum;
     }
 }
