4419d6713ce18ace8df608f56dda59b07ea18f0e
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-06-11 16:25:55 -0400

    Time: 125 ms (31.67%) | Memory: 56.1 MB (5.02%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,25 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-     int n = s.length();
+    int n = s.length();
+     int[][] dp = new int[n][n];
+     for(int[] row: dp) java.util.Arrays.fill(row, -1); 
+     if(s.length()<2) return s;
      String max = s.substring(0,1);
      for(int i=0;i<n;i++)
      {
-        String odd = ispal(s, i, i);
-        if(odd.length()>max.length())
-        max = odd;
-        String even = ispal(s, i, i+1);
-        if(even.length()>max.length())
-        max = even;
+        for(int j=i+max.length();j<n;j++)
+        {
+            if(ispal(s,i,j,dp)==1)
+            {
+                if(j-i+1>max.length())
+                max=s.substring(i,j+1);
+            }
+        }
      }
      return max;
     }
-    private String ispal(String s, int i , int j){
-        int n=s.length();
-        while(i>=0 && j<n && s.charAt(i)==s.charAt(j))
-        {
-            i--;
-            j++;
-        }
-        return s.substring(i+1,j);
+    private int ispal(String s, int i, int j, int[][] dp)
+    {
+        if(i>=j) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j))
+        return dp[i][j]=ispal(s, i+1, j-1, dp);
+        return 0;
     }
 }
