e3a513b646720a41fad227a5fac35921b4847031
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-06-12 18:00:14 -0400

    Time: 4 ms (3.32%) | Memory: 44.9 MB (88.32%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Node{
+        TreeNode node;
+        int level;
+        Node(TreeNode node, int level)
+        {
+            this.node=node;
+            this.level=level;
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        Queue<Node> q=new LinkedList<>();
+        q.add(new Node(root, 0));
+        TreeMap<Integer, List<Integer>> map=new TreeMap<>();
+        while(!q.isEmpty())
+        {
+            Node p=q.poll();
+            map.computeIfAbsent(p.level, k-> new ArrayList<>())
+               .add(p.node.val);
+            if(p.node.left!=null)
+            q.add(new Node(p.node.left, p.level+1));
+            if(p.node.right!=null)
+            q.add(new Node(p.node.right, p.level+1));
+        }
+        List<List<Integer>> res=new ArrayList<>();
+        for(List<Integer> val:map.values())
+        res.add(val);
+        return res;
+    }
+}
