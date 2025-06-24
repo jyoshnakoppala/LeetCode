c630f1727e415eacb540b7d9671a4d7b8e5045ba
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-06-24 15:02:00 -0400

    Time: 1 ms (99.77%) | Memory: 44.9 MB (74.83%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -2,54 +2,49 @@ import java.util.*;
 
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][n];
-
-        // Initialize board with '.'
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+        List<List<String>> res=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(board[i],'.');
         }
+        int[] leftrow=new int[n];
+        int[] upper=new int[2*n-1];
+        int[] lower=new int[2*n-1];
 
-        int[] leftRow = new int[n];
-        int[] upperDiagonal = new int[2 * n - 1];
-        int[] lowerDiagonal = new int[2 * n - 1];
-
-        solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
-        return ans;
+        solve(0, board, res, leftrow, upper, lower, n);
+        return res;
     }
-
-    private void solve(int col, char[][] board, List<List<String>> ans,
-                       int[] leftRow, int[] upperDiagonal, int[] lowerDiagonal, int n) {
-        if (col == n) {
-            // Convert board to List<String> and add to ans
-            List<String> res = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                res.add(new String(board[i]));
+    private void solve(int col, char[][] board, List<List<String>> res, int[] left, int[] upper, int[] lower, int n)
+    {
+        if(col==n)
+        {
+            List<String> path=new ArrayList<>();
+            for(int i=0;i<n;i++)
+            {
+                path.add(new String(board[i]));
             }
-            ans.add(res);
+            res.add(path);
             return;
         }
+        for(int row=0;row<n;row++)
+        {
+            if (left[row] == 0 &&
+                lower[row + col] == 0 &&
+                upper[n - 1 + col - row] == 0)
+                {
+                    board[row][col]='Q';
+                    left[row] = 1;
+                lower[row + col] = 1;
+                upper[n - 1 + col - row] = 1;
 
-        for (int row = 0; row < n; row++) {
-            if (leftRow[row] == 0 &&
-                lowerDiagonal[row + col] == 0 &&
-                upperDiagonal[n - 1 + col - row] == 0) {
+                solve(col + 1, board, res, left, upper, lower, n);
 
-                // Place the queen
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
-
-                // Recurse
-                solve(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
-
-                // Backtrack
                 board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
-            }
+                left[row] = 0;
+                lower[row + col] = 0;
+                upper[n - 1 + col - row] = 0;
+                }
         }
     }
 }
