e276016c6310d5d7dae835b13710eb8d23fccbf7
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-07-17 16:53:19 -0400

    Time: 19 ms (39.58%) | Memory: 45.2 MB (25.34%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,35 +1,31 @@
 class Solution {
-    private int findans(int i, int[] coins, int target, int[][] dp)
-    {
-        if(i==0)
-        {
-        if(target%coins[0]==0)
-        return target/coins[0];
-        else
-        return Integer.MAX_VALUE;
-        }
-
-        if(dp[i][target]!=-1) return dp[i][target];
-
-        int nottake=findans(i-1, coins, target, dp);
-        int take=Integer.MAX_VALUE;
-        if(coins[i]<=target) 
-        {
-        int res = findans(i, coins, target - coins[i], dp);
-            if (res != Integer.MAX_VALUE)  
-                take = 1 + res;
-        }
-
-        return dp[i][target]=Math.min(take, nottake);
-    }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int i=0;i<n;i++)
+
+        for(int j=0;j<amount+1;j++)
         {
-            Arrays.fill(dp[i], -1);
+        if(j%coins[0]==0)
+        dp[0][j] = j/coins[0];
+        else dp[0][j] = Integer.MAX_VALUE;
         }
-        int ans= findans(n-1, coins, amount, dp);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<amount+1;j++)
+            {
+
+                int nottake=dp[i-1][j];
+                int take=Integer.MAX_VALUE;
+                if(coins[i]<=j) 
+                 {
+                    int res = dp[i][j-coins[i]];
+                    if (res != Integer.MAX_VALUE)  
+                    take = 1 + res;
+                 }
+                 dp[i][j]=Math.min(take, nottake);
+            }
+        }
+        int ans=dp[n-1][amount];
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
