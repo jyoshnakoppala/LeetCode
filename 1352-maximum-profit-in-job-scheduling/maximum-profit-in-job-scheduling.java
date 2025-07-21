8a90465ebc3708d028c258737d68020b1357bba7
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-07-21 16:27:32 -0400

    Time: 30 ms (83.28%) | Memory: 57.5 MB (41.74%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,19 +1,9 @@
 class Solution {
-    private int findans(int[][] jobs, int[] sorted, int ind, int[] dp)
-    {
-        if(ind==jobs.length) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int nextIndex = binarySearch(sorted, jobs[ind][1]);
-        int take=jobs[ind][2]+findans(jobs, sorted, nextIndex, dp);
-        int nottake=findans(jobs, sorted, ind+1, dp);
-        return dp[ind]=Math.max(take, nottake);
-    }
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
       int n=profit.length;
       int[][] jobs=new int[n][3];
 
-      int[] dp=new int[n];
-      Arrays.fill(dp, -1);
+      int[] dp=new int[n+1];
 
       for(int i=0;i<n;i++)
       {
@@ -26,7 +16,16 @@ class Solution {
       for(int i=0;i<n;i++)
       sorted[i]=jobs[i][0];
 
-      return findans(jobs, sorted, 0, dp);
+      dp[n]=0;
+
+      for(int i=n-1;i>=0;i--)
+      {
+        int nextIndex = binarySearch(sorted, jobs[i][1]);
+        int take=jobs[i][2]+dp[nextIndex];
+        int nottake=dp[i+1];
+        dp[i]=Math.max(take, nottake);
+      }
+      return dp[0];
     }
     private int binarySearch(int[] start, int target) {
         int low = 0, high = start.length;
