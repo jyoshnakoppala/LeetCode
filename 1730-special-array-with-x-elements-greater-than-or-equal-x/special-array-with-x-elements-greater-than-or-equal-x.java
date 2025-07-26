523eafd92c52a90ec45a326b2da74ddc13e1befd
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-07-26 15:06:24 -0400

    Time: 1 ms (79.79%) | Memory: 41.8 MB (17.90%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,41 @@
+class Solution {
+    private int upperbound(int[] nums, int x)
+    {
+        int ans=nums.length;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]>=x)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            low=mid+1;
+        }
+        return ans;
+    }
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        int n=nums.length;
+        int low=1;
+        int high=nums.length;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            count=n-upperbound(nums, mid);
+            if(count==mid)
+            return count;
+
+            else if(count>mid)
+            low=mid+1;
+            else
+            high=mid-1;
+        }
+        return -1;
+    }
+}
