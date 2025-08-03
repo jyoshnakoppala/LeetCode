b5c90931eb74e2955b81ac241336aaed1b117399
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-08-03 18:28:15 -0400

    Time: 14 ms (55.17%) | Memory: 54.8 MB (14.75%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,13 +1,35 @@
 class MyHashSet {
 
-    private final boolean[] present;
+    private static final int SIZE = 769;              
+    private final LinkedList<Integer>[] buckets;
 
     public MyHashSet() {
-        present = new boolean[1_000_001];
+        buckets = new LinkedList[SIZE];
     }
-    public void add(int key) { present[key] = true; }
-    public void remove(int key) { present[key] = false; }
-    public boolean contains(int key) { return present[key]; }
+
+    public void add(int key) { 
+        int i=key%SIZE;
+        if (buckets[i] == null) {
+            buckets[i] = new LinkedList<>();
+        }
+        if (!buckets[i].contains(key)) {
+            buckets[i].add(key);
+        }
+     }
+
+    public void remove(int key) {
+        int i=key%SIZE;
+        LinkedList<Integer> list=buckets[i];
+        int temp;
+        if(list==null) return;
+        list.remove(Integer.valueOf(key));
+    }
+    public boolean contains(int key) { 
+        int i=key%SIZE;
+        LinkedList<Integer> list=buckets[i];
+        if(list==null) return false;;
+        return list.contains(key);
+    }
 }
 
 /**
