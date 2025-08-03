08557730ddde03c623ea3db1c2f3cb93b0047a64
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-08-03 17:52:44 -0400

    Time: 17 ms (73.50%) | Memory: 48.1 MB (81.77%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,23 +1,71 @@
 class MyHashMap {
-    private final int[] map;
+    private static final int SIZE=769;
+    private final LinkedList<Node>[] buckets;
     public MyHashMap() {
-        map=new int[1_000_001];
-        Arrays.fill(map, -1);
+        buckets=new LinkedList[SIZE];
     }
     
     public void put(int key, int value) {
-        map[key]=value;
+        int i=key%SIZE;
+        LinkedList<Node> bucket=buckets[i];
+        if(bucket==null) 
+        {
+        buckets[i]=new LinkedList<>();
+        buckets[i].add(new Node(key, value));
+        return;
+        }
+        else
+        {
+            for(Node n:bucket)
+            {
+                if(n.key==key) 
+                {
+                n.value=value;
+                return;
+                }
+            }
+        }
+        buckets[i].add(new Node(key, value));
     }
     
     public int get(int key) {
-        return map[key];
+        int i=key%SIZE;
+        LinkedList<Node> bucket=buckets[i];
+        if(bucket==null) return -1;
+        else
+        {
+            for(Node n:bucket)
+            {
+                if(n.key==key) return n.value;
+            }
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        map[key]=-1;
+        int i=key%SIZE;
+        LinkedList<Node> bucket=buckets[i];
+        Node toremove=null;
+        if(bucket==null) return;
+        else
+        {
+            for(Node n:bucket)
+            {
+                if(n.key==key) toremove=n;
+            }
+            if(toremove!=null) bucket.remove(toremove);
+        }
     }
 }
-
+class Node{
+    int key;
+    int value;
+    Node(int key, int value)
+    {
+        this.key=key;
+        this.value=value;
+    }
+}
 /**
  * Your MyHashMap object will be instantiated and called as such:
  * MyHashMap obj = new MyHashMap();
