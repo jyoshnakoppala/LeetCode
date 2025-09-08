04790a70061735411d81688e4f9c23a0f362abb4
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-09-08 13:41:47 -0400

    Time: 4 ms (92.95%) | Memory: 46 MB (51.72%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,32 +1,20 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int i=0;
-        int j=k-1;
-        int sum=0;
-        int ans=Integer.MAX_VALUE;
-        int start=0;
-        int end=0;
-        ArrayList<Integer> finalans=new ArrayList<>();
-        while(j<arr.length)
+        int left=0;
+        int right=arr.length-k;
+        while(left<right)
         {
-            for(int ind=i;ind<=j;ind++)
-            {
-                sum+=Math.abs(x-arr[ind]);
-            }
-            if(sum<ans)
-            {
-                ans=sum;
-                start=i;
-                end=j;
-            }
-            sum=0;
-            i++;
-            j++;
+            int mid=(left+right)/2;
+            if(x-arr[mid]>arr[mid+k]-x)
+            left=mid+1;
+            else
+            right=mid;
         }
-        for(int l=start;l<=end;l++)
+        List<Integer> result=new ArrayList<>();
+        for(int i=left;i<left+k;i++)
         {
-            finalans.add(arr[l]);
+            result.add(arr[i]);
         }
-        return finalans;
+        return result;
     }
 }
