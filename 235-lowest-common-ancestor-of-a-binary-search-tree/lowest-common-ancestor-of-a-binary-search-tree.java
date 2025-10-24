8b286f5b8c1a85c49609ad6b4b686a003fd51113
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-10-24 19:25:57 -0400

    Time: 6 ms (27.32%) | Memory: 45.4 MB (26.83%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,31 +10,16 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> arrayp=new ArrayList<>();
-        ArrayList<TreeNode> arrayq=new ArrayList<>();
+        if(root==null) return null;
 
-        findpath(root, p, arrayp);
-        findpath(root, q, arrayq);
+        if(root==p) return p;
+        if(root==q) return q; 
 
-        int i=0;
-        while(i<arrayp.size() && i<arrayq.size() && arrayp.get(i)==arrayq.get(i))
-        {
-            i++;
-        }
-        return arrayp.get(i-1);
-    }
-    private boolean findpath(TreeNode root, TreeNode find, List<TreeNode> res)
-    {
-        if(root==null) return false;
+        TreeNode left=lowestCommonAncestor(root.left, p, q);
+        TreeNode right=lowestCommonAncestor(root.right, p, q);
 
-        res.add(root);
-
-        if(root.val==find.val) return true;
-
-        if(findpath(root.left, find, res)|| findpath(root.right, find, res))
-        return true;
-
-        res.remove(root);
-        return false;
+        if(right!=null && left!=null) return root;
+        else if(left!=null) return left;
+        else return right;
     }
 }
