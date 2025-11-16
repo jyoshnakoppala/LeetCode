6b9c144efa824d260b679c5faaccccea8382cbd5
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2025-11-16 15:42:06 -0500

    Time: 19 ms (42.66%) | Memory: 98.1 MB (7.12%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> combine(int n, int k) {
+       findans(1, n, k, new ArrayList<>());
+       return res; 
+    }
+    private void findans(int ind, int n, int k, List<Integer> ds)
+    {
+        if(ds.size()==k)
+        {
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=ind;i<=n;i++)
+        {
+            ds.add(i);
+            findans(i+1, n, k, ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
