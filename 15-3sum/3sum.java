4829eef11448f4d924dc06acd32307f51315e09c
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2026-04-17 09:04:57 -0400

    Time: 31 ms (82.85%) | Memory: 59.1 MB (77.48%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-      List<List<Integer>> list=new ArrayList<>();
-      int n=nums.length;
-      Arrays.sort(nums);
-      for(int i=0;i<n;i++)
-      {
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        int j=i+1;
-        int k=n-1;
-        while(j<k)
-        {
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum>0)
-            {
-                k--;
-            }
-            else if(sum<0)
-            j++;
-            else
-            {
-                list.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1]) j++;
-                while(j<k && nums[k]==nums[k+1]) k--;
-            }
+        List<List<Integer>> result=new ArrayList<>();
+        int n=nums.length;
+        if(n<3){
+            return result;
         }
-      }
-      return list;
+        Arrays.sort(nums);
+        for(int i=0; i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+
+            int target=-nums[i];
+            int left=i+1;
+            int right=n-1;
+
+            while(left<right){
+                int sum=nums[left]+nums[right];
+                if(sum==target){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    int currentLeft=nums[left];
+                    while(left<right && nums[left]==currentLeft)
+                    left++;
+
+                    int currentRight=nums[right];
+                    while(left<right && nums[right]==currentRight)
+                    right--;
+                }
+                else if(sum< target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+        }
+        return result;
     }
 }
