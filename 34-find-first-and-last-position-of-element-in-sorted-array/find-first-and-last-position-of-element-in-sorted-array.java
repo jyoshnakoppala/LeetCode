22a6ff65bda26d29bbb141909921425c0ba8131d
jyoshnakoppala <103808699+jyoshnakoppala@users.noreply.github.com>
2026-04-17 12:26:30 -0400

    Time: 0 ms (100.00%) | Memory: 48 MB (88.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,50 +1,43 @@
 class Solution {
-    private int lowerbound(int[] nums, int x)
-    {
-        int ans=-1;
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(nums[mid]==x)
-            {
-                ans=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<x)
-            low=mid+1;
-            else
-            high=mid-1;
-        }
-        return ans;
-    }
-    private int upperbound(int[] nums, int x)
-    {
-        int ans=-1;
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(nums[mid]==x)
-            {
-                ans=mid;
-                low=mid+1;
-            }
-            else if (nums[mid] < x) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
-    }
     public int[] searchRange(int[] nums, int target) {
-        int[] ans=new int[2];
-        ans[0]=lowerbound(nums, target);
-        ans[1]=upperbound(nums, target);
-
-        return ans;
+        int first=findBound(nums, target, true);
+        if(first==-1)
+        {
+            return new int[]{-1, -1};
+        }
+        int last=findBound(nums, target, false);
+        return new int[]{first, last};
     }
+        private int findBound(int[] nums, int target, Boolean findfirst)
+        {
+            int left=0;
+            int right=nums.length-1;
+            int result=-1;
+
+            while(left<=right)
+            {
+                int mid=left+(right-left)/2;
+                if(nums[mid]==target)
+                {
+                    result=mid;
+                    if(findfirst)
+                    {
+                        right=mid-1;
+                    }
+                    else{
+                        left=mid+1;
+                    }
+                }
+                else if(nums[mid]<target)
+                {
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
+            }
+            return result;
+        }
 }
+        
